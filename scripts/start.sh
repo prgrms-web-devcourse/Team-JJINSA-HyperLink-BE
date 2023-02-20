11f3e22455f3dac59934e400e2e1f76f8513a703
ybkim-dev <beanybigggy@gmail.com>
2023-02-21 04:51:44 +0900

    feat : dev 환경 CD 구축

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,13 @@
-REPOSITORY="/home/ec2-user"
+REPOSITORY="/home/ec2-user/app"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+cd /home/ec2-user
+cp application.yml $REPOSITORY
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 sudo chmod +x $JAR_NAME
 
-nohup java -jar -Dspring.profiles.active=dev $JAR_NAME --server.port=5000 \
+nohup java -jar -Dspring.profiles.active=set1 $JAR_NAME  \
 $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
