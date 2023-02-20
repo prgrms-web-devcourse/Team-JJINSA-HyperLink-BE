1fee183c23f9a1fc65e78e286a99f9c6b811ca88
ybkim-dev <beanybigggy@gmail.com>
2023-02-21 05:11:43 +0900

    fix : dev 환경 CD 구성 완료

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,9 +2,6 @@ REPOSITORY="/home/ec2-user/app"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-cd /home/ec2-user
-cp application.yml $REPOSITORY
-
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 sudo chmod +x $JAR_NAME
