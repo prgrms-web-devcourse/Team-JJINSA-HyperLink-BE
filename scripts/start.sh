6167dd02f96e067eb970e1d987ad77e71712e210
ybkim-dev <beanybigggy@gmail.com>
2023-02-23 02:05:35 +0900

    fix : https로 변동되면서 CI CD 문제 해결
    - https를 적용하면서 https로 ip를 통해 접근을 못하는 문제 발생
    - 이를 해결함

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,16 @@ BASE_PATH=/home/ec2-user/app
 CHECK_PROFILE=/api/v0.1/profile
 CHECK_HEALTH=/api/v0.1/actuator/health
 
-cp $BASE_PATH/zip/*.jar $BASE_PATH/
+MY_IP=$(curl -s ifconfig.me)
+if [ $MY_IP == 54.180.58.103 ]
+then
+  MY_DOMAIN=test.hyper-link.store
+elif [ $MY_IP == 54.180.208.136 ]
+then
+  MY_DOMAIN=api.hyper-link.store
+fi
+
+sudo cp $BASE_PATH/zip/*.jar $BASE_PATH/
 
 BUILD_PATH=$(ls $BASE_PATH/*.jar | tail -n 1)
 JAR_NAME=$(basename $BUILD_PATH)
@@ -14,8 +23,8 @@ DEPLOY_PATH=$BASE_PATH/jar/
 
 sudo cp $JAR_NAME $DEPLOY_PATH
 
-echo "> 현재 구동중인 Set 확인"
-CURRENT_PROFILE=$(curl -s http://localhost$CHECK_PROFILE)
+echo "> 현재 구동중인 Set 확인(curl -s https://$MY_DOMAIN$CHECK_PROFILE)"
+CURRENT_PROFILE=$(curl -s https://$MY_DOMAIN$CHECK_PROFILE)
 echo "> $CURRENT_PROFILE"
 
 # 쉬고 있는 set 찾기: set1이 사용중이면 set2가 쉬고 있고, 반대면 set1이 쉬고 있음
@@ -33,7 +42,7 @@ else
   IDLE_PROFILE=set1
   IDLE_PORT=8000
 fi
-
+echo "> IDLE_PROFLE : $IDLE_PROFILE"
 echo "> application.jar 교체"
 IDLE_APPLICATION=$IDLE_PROFILE-server-0.0.1-SNAPSHOT.jar
 IDLE_APPLICATION_PATH=$DEPLOY_PATH$IDLE_APPLICATION
@@ -56,12 +65,12 @@ echo "> $IDLE_PROFILE 배포"
 sudo nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $IDLE_APPLICATION_PATH &
 
 echo "> $IDLE_PROFILE 10초 후 Health check 시작"
-echo "> curl -s http://localhost:$IDLE_PORT$CHECK_HEALTH "
+echo "> curl -s http://$MY_DOMAIN:$IDLE_PORT$CHECK_HEALTH "
 sleep 10
 
 for retry_count in {1..10}
 do
-  response=$(curl -s http://localhost:$IDLE_PORT$CHECK_HEALTH)
+  response=$(curl -s http://$MY_DOMAIN:$IDLE_PORT$CHECK_HEALTH)
   up_count=$(echo $response | grep 'UP' | wc -l)
 
   if [ $up_count -ge 1 ]
@@ -83,6 +92,3 @@ do
   echo "> Health check 연결 실패. 재시도..."
   sleep 10
 done
-
-do
-  sh /home/ec2-user/app/switch.sh
--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+MY_IP=$(curl -s ifconfig.me)
+if [ $MY_IP == 54.180.58.103 ]
+then
+  MY_DOMAIN=test.hyper-link.store
+elif [ $MY_IP == 54.180.208.136 ]
+then
+  MY_DOMAIN=api.hyper-link.store
+fi
+
 CHECK_PROFILE=/api/v0.1/profile
 echo "> 현재 구동중인 Port 확인"
-CURRENT_PROFILE=$(curl -s http://localhost$CHECK_PROFILE)
+CURRENT_PROFILE=$(curl -s https://$MY_DOMAIN$CHECK_PROFILE)
 
 # 쉬고 있는 set 찾기: set1이 사용중이면 set2가 쉬고 있고, 반대면 set1이 쉬고 있음
 if [ $CURRENT_PROFILE == set1 ]
@@ -21,7 +30,7 @@ echo "> 전환할 Port: $IDLE_PORT"
 echo "> Port 전환"
 echo "set \$service_url http://127.0.0.1:$IDLE_PORT;" | sudo tee /etc/nginx/conf.d/service-url.inc
 
-PROXY_PORT=$(curl -s http://localhost$CHECK_PROFILE)
+PROXY_PORT=$(curl -s http://$MY_DOMAIN$CHECK_PROFILE)
 echo "> Nginx Current Proxy Port: $PROXY_PORT"
 
 echo "> Nginx Reload"
