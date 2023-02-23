0e390f18c075e63356fca19314920883ab06998c
yeezze <yeeeze22@gmail.com>
2023-02-23 22:48:43 +0900

    test: 조회수 갱신 API test 작성

--- src/test/java/com/hyperlink/server/content/application/ContentServiceIntegrationTest.java
+++ src/test/java/com/hyperlink/server/content/application/ContentServiceIntegrationTest.java
@@ -7,11 +7,21 @@ import com.hyperlink.server.domain.content.application.ContentService;
 import com.hyperlink.server.domain.content.domain.ContentRepository;
 import com.hyperlink.server.domain.content.domain.entity.Content;
 import com.hyperlink.server.domain.content.exception.ContentNotFoundException;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -33,13 +43,13 @@ public class ContentServiceIntegrationTest {
     void success() {
       Content content = new Content("title", "contentImgUrl", "link");
       contentRepository.save(content);
+      int inquiryCountBeforeAdd = content.getInquiry();
 
-      content.addInquiry();
-      int inquiryCountAfterAdd = content.getInquiry();
+      contentService.addInquiryAndGetCount(content.getId());
 
       int findInquiry = contentService.getInquiry(content.getId());
 
-      assertThat(findInquiry).isEqualTo(inquiryCountAfterAdd);
+      assertThat(findInquiry).isEqualTo(inquiryCountBeforeAdd + 1);
     }
 
     @Test
@@ -49,5 +59,69 @@ public class ContentServiceIntegrationTest {
         contentService.getInquiry(0L);
       });
     }
+  }
+
+  @TestInstance(Lifecycle.PER_CLASS)
+  @TestMethodOrder(OrderAnnotation.class)
+  @Nested
+  @DisplayName("조회수 추가 메서드는")
+  class AddInquiryAndGetCount {
+
+    Content content;
+    final int memberCount = 3;
+    int beforeInquiry;
+
+    @Order(1)
+    @Rollback(value = false)
+    @Test
+    @DisplayName("3명의 사용자가 동시에 조회수 추가를 요청했을 때")
+    void manyRequestInquiry() throws InterruptedException {
+      final CountDownLatch countDownLatch = new CountDownLatch(memberCount);
+
+      content = new Content("title", "contentImgUrl", "link");
+      content = contentRepository.save(content);
+      Long contentId = content.getId();
+      beforeInquiry = content.getInquiry();
+
+      List<Thread> workers = Stream.generate(() -> new Thread(new Worker(countDownLatch, contentId)))
+          .limit(memberCount)
+          .toList();
+      workers.forEach(Thread::start);
+      countDownLatch.await(5, TimeUnit.SECONDS);
+    }
+
+    @Order(2)
+    @Test
+    @DisplayName("조회수가 +3 처리된다.")
+    void addInquiry() {
+      Content findContent = contentRepository.findById(content.getId())
+          .orElseThrow(ContentNotFoundException::new);
+      assertThat(findContent.getInquiry()).isEqualTo(beforeInquiry + memberCount);
+    }
+
+    @Order(3)
+    @Rollback(value = false)
+    @Test
+    void tearDownContent() {
+      contentRepository.deleteById(content.getId());
+    }
+
+    private class Worker implements Runnable {
+
+      private CountDownLatch countDownLatch;
+      private Long contentId;
+
+      public Worker(CountDownLatch countDownLatch, Long contentId) {
+        this.countDownLatch = countDownLatch;
+        this.contentId = contentId;
+      }
+
+      @Override
+      public void run() {
+        contentService.addInquiryAndGetCount(contentId);
+        countDownLatch.countDown();
+      }
+    }
+
   }
 }
