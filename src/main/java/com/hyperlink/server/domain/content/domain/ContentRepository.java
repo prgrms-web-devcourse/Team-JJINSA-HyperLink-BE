06e548ef70d5cc4cf197a6dec16257d34e7e334b
yeezze <yeeeze22@gmail.com>
2023-02-23 23:29:45 +0900

    fix: sql 파라미터 누락 오류 수정

--- src/main/java/com/hyperlink/server/domain/content/domain/ContentRepository.java
+++ src/main/java/com/hyperlink/server/domain/content/domain/ContentRepository.java
@@ -4,11 +4,12 @@ import com.hyperlink.server.domain.content.domain.entity.Content;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ContentRepository extends JpaRepository<Content, Long> {
 
   @Modifying(clearAutomatically = true)
-  @Query("update Content c set c.inquiry = c.inquiry + 1")
-  int updateInquiryCount(Long contentId);
+  @Query("update Content c set c.inquiry = c.inquiry + 1 where c.id = :content_id")
+  int updateInquiryCount(@Param("content_id") Long contentId);
 
 }
