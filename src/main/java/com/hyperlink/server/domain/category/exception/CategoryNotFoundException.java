3c0e8717a580059950345f8af5efccbe8bc00e91
ybkim-dev <beanybigggy@gmail.com>
2023-02-28 02:14:03 +0900

    feat : Category 이름으로 검색 로직 작성
    - Category를 이름으로 검색하여 존재하지 않으면 사용자가 입력한 카테고리명에 이상이 있는 것으로 판단하여 에러를 던진다.

---
+++ src/main/java/com/hyperlink/server/domain/category/exception/CategoryNotFoundException.java
@@ -0,0 +1,14 @@
+package com.hyperlink.server.domain.category.exception;
+
+import com.hyperlink.server.global.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+
+public class CategoryNotFoundException extends BusinessException {
+
+  private static final String MESSAGE = "카테고리를 찾을 수 없습니다.";
+  private static final HttpStatus status = HttpStatus.NOT_FOUND;
+
+  public CategoryNotFoundException() {
+    super(MESSAGE, status);
+  }
+}
