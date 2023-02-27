02b09e32c4266adcbfb88ecb40d571d81bc01584
yeezze <yeeeze22@gmail.com>
2023-02-27 15:53:16 +0900

    feat: GlobalExceptionHandler validationException 추가

--- src/main/java/com/hyperlink/server/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hyperlink/server/global/exception/GlobalExceptionHandler.java
@@ -1,21 +1,26 @@
 package com.hyperlink.server.global.exception;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+  @ExceptionHandler(ValidationException.class)
+  public ResponseEntity<ErrorResponse> handleValidationException(ValidationException e) {
+    ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+    return ResponseEntity.badRequest().body(errorResponse);
+  }
 
   @ExceptionHandler(BusinessException.class)
   protected ResponseEntity<String> handledException(BusinessException e) {
