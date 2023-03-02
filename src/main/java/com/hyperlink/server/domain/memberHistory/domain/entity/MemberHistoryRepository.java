7a3a47b6ce35c5152d45bfc54ccbe505222a2d26
yeezze <yeeeze22@gmail.com>
2023-03-02 18:22:42 +0900

    feat: 히스토리 전체 조회 API 구현

--- src/main/java/com/hyperlink/server/domain/memberHistory/domain/entity/MemberHistoryRepository.java
+++ src/main/java/com/hyperlink/server/domain/memberHistory/domain/entity/MemberHistoryRepository.java
@@ -1,9 +1,13 @@
 package com.hyperlink.server.domain.memberHistory.domain.entity;
 
 import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberHistoryRepository extends JpaRepository<MemberHistory, Long> {
 
   List<MemberHistory> findAllByMemberId(Long memberId);
+
+  Slice<MemberHistory> findSliceByMemberId(Long memberId, Pageable pageable);
 }
