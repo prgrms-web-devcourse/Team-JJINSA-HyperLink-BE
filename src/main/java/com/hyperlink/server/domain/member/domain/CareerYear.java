7f2f54c29fc0d7a82e088c8d061cc56c79b830a9
kivv00ng <rldnd2637@naver.com>
2023-03-03 17:13:52 +0900

    refactore: CategoryYear value 및 네이밍 변경

--- src/main/java/com/hyperlink/server/domain/member/domain/CareerYear.java
+++ src/main/java/com/hyperlink/server/domain/member/domain/CareerYear.java
@@ -4,18 +4,18 @@ import com.hyperlink.server.domain.member.exception.CareerYearNotFoundException;
 import java.util.stream.Stream;
 
 public enum CareerYear {
-  LESS_ONE("less1"),
-  ONE("1"),
-  TWO("2"),
-  THREE("3"),
-  FOUR("4"),
-  FIVE("5"),
-  SIX("6"),
-  SEVEN("7"),
-  EIGHT("8"),
-  NINE("9"),
-  TEN("10"),
-  MORE_TEN("more10");
+  LESS_THAN_ONE("lessThanOne"),
+  ONE("one"),
+  TWO("two"),
+  THREE("three"),
+  FOUR("four"),
+  FIVE("five"),
+  SIX("six"),
+  SEVEN("seven"),
+  EIGHT("eight"),
+  NINE("nine"),
+  TEN("ten"),
+  MORE_THAN_TEN("moreThanTen");
 
   private final String value;
 
