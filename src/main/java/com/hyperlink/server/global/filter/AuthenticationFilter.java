0900f928e5f93c8038d97eaba53cbfb205e6eb05
ybkim-dev <beanybigggy@gmail.com>
2023-03-05 15:40:56 +0900

    feat : contents 조회 whitelist 추가

--- src/main/java/com/hyperlink/server/global/filter/AuthenticationFilter.java
+++ src/main/java/com/hyperlink/server/global/filter/AuthenticationFilter.java
@@ -22,7 +22,7 @@ public class AuthenticationFilter implements Filter {
 
   private static final String[] whitelist = {"/", "/members/logout", "/members/login",
       "/members/signup", "/profile", "/actuator/health", "/members/oauth/code/google",
-      "/members/access-token"};
+      "/members/access-token", "/contents"};
 
   private final AuthTokenExtractor authTokenExtractor;
   private final JwtTokenProvider jwtTokenProvider;
