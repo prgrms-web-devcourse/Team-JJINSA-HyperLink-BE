0c458aa112602d503ece8cc7733a42f1f1e314de
yeezze <yeeeze22@gmail.com>
2023-03-12 23:54:46 +0900

    refactor: @EnableBatchProcessing 위치 변경

--- src/main/java/com/hyperlink/server/ServerApplication.java
+++ src/main/java/com/hyperlink/server/ServerApplication.java
@@ -1,12 +1,10 @@
 package com.hyperlink.server;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableBatchProcessing
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
