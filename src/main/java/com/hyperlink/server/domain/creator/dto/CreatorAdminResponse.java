e8dd02b95d0ffff74ce8af855b9cdc170e53b6ab
ybkim-dev <beanybigggy@gmail.com>
2023-03-13 22:30:33 +0900

    fix : 어드민 페이지 크리에이터 전체 조회 필드 추가

--- src/main/java/com/hyperlink/server/domain/creator/dto/CreatorAdminResponse.java
+++ src/main/java/com/hyperlink/server/domain/creator/dto/CreatorAdminResponse.java
@@ -2,9 +2,10 @@ package com.hyperlink.server.domain.creator.dto;
 
 import com.hyperlink.server.domain.creator.domain.entity.Creator;
 
-public record CreatorAdminResponse(Long creatorId, String name, String description, String categoryName) {
+public record CreatorAdminResponse(Long creatorId, String name, String description, String categoryName,
+                                   String profileImgUrl) {
   public static CreatorAdminResponse from(Creator creator) {
     return new CreatorAdminResponse(creator.getId(), creator.getName(), creator.getDescription(),
-        creator.getCategoryName());
+        creator.getCategoryName(), creator.getProfileImgUrl());
   }
 }
